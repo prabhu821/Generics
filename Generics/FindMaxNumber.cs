705692f06fde7a19282be17586f8777bf2d9679c
rishiraj007 <rishiraj@mounttalent.com>
2022-12-13 17:12:43 +0530

    [Prabhu]: Find Max Float

--- Generics/FindMaxNumber.cs
+++ Generics/FindMaxNumber.cs
@@ -8,7 +8,26 @@ namespace Generics
 {
     internal class FindMaxNumber
     {
+        //MAX INTEGER NUMBER 
         public static int MaxIntegerNumber(int a, int b, int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+            {
+                return a;
+            }
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+            {
+                return b;
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0)
+            {
+                return c;
+            }
+            return a;
+        }
+
+        //MAX FLOAT NUMBER
+        public static double MaxFloatNumber(double a, double b, double c)
         {
             if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
             {
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,6 +8,9 @@
 
             int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
             Console.WriteLine(output);
+
+            double output1 = FindMaxNumber.MaxFloatNumber(10.2, 3.3, 5.4);
+            Console.WriteLine(output1);
         }
     }
 }
