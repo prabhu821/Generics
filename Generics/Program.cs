b6fefb51638b54726dd8cc468eddc7c6f009fd5c
rishiraj007 <rishiraj@mounttalent.com>
2022-12-13 17:03:32 +0530

    [Prabhu]: Find Max Integer

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Find Max Number");
+
+            int output = FindMaxNumber.MaxIntegerNumber(10, 3, 5);
+            Console.WriteLine(output);
         }
     }
 }
