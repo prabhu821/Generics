0a49ebe5762ad80beb0ff940aa0b569313623006
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 20:00:42 +0530

    [Prabhu]Add: given 3 floats find maximum

--- Generics/FindMaximum.cs
+++ Generics/FindMaximum.cs
@@ -36,6 +36,35 @@ namespace Generics
             }
             return default(int);
         }
+
+        public static float FindMaxFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+
+               num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+
+               num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+
+               num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default(float);
+        }
     }
 }
 
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,13 +8,17 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Exit");
+                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Find Float Max \n3.Exit");
                 int options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
                     case 1:
                         int result = FindMaximum.FindMax(23, 25, 27);
                         Console.WriteLine("Max Integer is: " + result);
+                        break;
+                    case 2:
+                        float result1 = FindMaximum.FindMaxFloat(22.4f, 23.4f, 21.5F);
+                        Console.WriteLine("Max Float is: " + result1);
                         break;
                     default:
                         flag = false;
