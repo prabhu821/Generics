bd9fec08e2c7f45a34cdb5ef2916008e2e4bdfb4
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 20:13:30 +0530

    [Prabhu]Add: given 3 strings find maximum

--- Generics/FindMaximum.cs
+++ Generics/FindMaximum.cs
@@ -65,6 +65,35 @@ namespace Generics
             }
             return default(float);
         }
+
+        public static string? FindMaxString(string name1, string name2, string name3)
+        {
+            if (name1.CompareTo(name2) > 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) >= 0 && name1.CompareTo(name2) > 0 ||
+
+               name1.CompareTo(name2) > 0 && name1.CompareTo(name2) >= 0)
+            {
+                return name1;
+            }
+            if (name2.CompareTo(name1) > 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) >= 0 && name2.CompareTo(name3) > 0 ||
+
+               name2.CompareTo(name1) > 0 && name2.CompareTo(name3) >= 0)
+            {
+                return name2;
+            }
+            if (name3.CompareTo(name1) > 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) >= 0 && name3.CompareTo(name2) > 0 ||
+
+               name3.CompareTo(name1) > 0 && name3.CompareTo(name2) >= 0)
+            {
+                return name3;
+            }
+            return default;
+        }
     }
 }
 
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Find Float Max \n3.Exit");
+                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Find Float Max \n3.Find String Max \n4.Exit");
                 int options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
@@ -19,6 +19,10 @@
                     case 2:
                         float result1 = FindMaximum.FindMaxFloat(22.4f, 23.4f, 21.5F);
                         Console.WriteLine("Max Float is: " + result1);
+                        break;
+                    case 3:
+                        string result2 = FindMaximum.FindMaxString("Apple","Banana","Peach");
+                        Console.WriteLine("Max String is: " + result2);
                         break;
                     default:
                         flag = false;
