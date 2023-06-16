492a414621db02ee979b000f4abde7559d8828a4
rishiraj007 <rishiraj@mounttalent.com>
2023-06-16 19:56:18 +0530

    [Prabhu]Add: given 3 integers find maximum

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Generics!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect option \n1.Find Integer Max \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        int result = FindMaximum.FindMax(23, 25, 27);
+                        Console.WriteLine("Max Integer is: " + result);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
